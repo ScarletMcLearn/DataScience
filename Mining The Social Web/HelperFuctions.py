6cfc55768c3306b51eebb957cd475f855182247e
ScarletMcLearn <ratul_shams@yahoo.com>
2018-02-25 14:54:02 +0600

    Add files via upload

--- Mining The Social Web/HelperFuctions.py
+++ Mining The Social Web/HelperFuctions.py
@@ -55,20 +55,11 @@
 
 
 # NEW TRY
-alltweets = [] 
+
 
 def get_all_tweets(name, screen_name):
-    #Twitter only allows access to a users most recent 3240 tweets with this method
+    alltweets = [] 
 
-    #authorize twitter, initialize tweepy
-#     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-#     auth.set_access_token(access_key, access_secret)
-#     api = tweepy.API(auth)
-
-    #initialize a list to hold all the tweepy Tweets
-#     alltweets[name] = [] 
-
-    #make initial request for most recent tweets (200 is the maximum allowed count)
     new_tweets = api.GetUserTimeline(screen_name = screen_name,count=200)
 
     #save most recent tweets
@@ -92,17 +83,8 @@ def get_all_tweets(name, screen_name):
     
         print ("...%s tweets downloaded so far" % (len(alltweets)))
 
-    #transform the tweepy tweets into a 2D array that will populate the csv	
-    outtweets = [[tweet.id_str, tweet.created_at, tweet.text.encode("utf-8")] for tweet in alltweets]
-
-    #write the csv	
-#     with open('%s_tweets.csv' % screen_name, 'wb') as f:
-#         writer = csv.writer(f)
-#         writer.writerow(["id","created_at","text"])
-#         writer.writerows(outtweets)
-
-    pass
-
+        return alltweets
+  
 
 
 import json
