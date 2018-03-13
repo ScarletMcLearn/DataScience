f8e3aa2bd8a239de479260866b5b6396f2a9d8be
ScarletMcLearn <ratul_shams@yahoo.com>
2018-03-13 06:08:20 +0600

    Add files via upload

--- Data Mining/Twitter Bot/HelperFuctions.py
+++ Data Mining/Twitter Bot/HelperFuctions.py
@@ -101,4 +101,90 @@ def jsonify(name, tweets):
 
 
 
+def jsonify_tweets(name, tweets):
+    data = {} 
+    data[name] = []  
+
+
+    for stat in tweets:
+        data[name].append(stat.AsJsonString())
+    
+    return data
+
+
+
 # SAVING THE JSON TWEETS
+
+
+
+
+
+#EXTRACTING IMAGES AND TEXT AS DICT FROM IMAGE CATG RESULT PAGE
+def txt_img_extract(res_page):
+    txt_img_dic = {}
+    try:
+        res_pg = requests.get(res_page)
+
+
+    except requests.ConnectionError:
+        print('Connection Error(s) occured. :( : ' + requests.ConnectionError)
+
+    res_page_soup = BeautifulSoup(res_pg.content, 'html5lib')
+
+    img_box = res_page_soup.findAll('div', {'class':'m-brick grid-item boxy bqQt'})
+
+    for itm in img_box:
+        try:
+            img_t = itm.find('img')['alt']
+            img_l = 'https://www.brainyquote.com' + itm.find('img')['src'].replace('.jpg', '-2x.jpg')
+
+            txt_img_dic[img_t] = img_l
+        except:
+            pass
+        
+    return txt_img_dic
+
+
+
+#CONVERT THE CRAPPED DIC TO LIST
+def txt_img_d2l(dict_t_i):
+    img_lst = []
+    for i in range(len(dict_t_i)):
+        img_lst.append([list(f.keys())[i], list(f.values())[i]])
+    return img_lst   
+
+
+
+
+
+#GETTING COMPLETE LIST
+def complete_img_l(img_lst):
+    for i in range(len(img_lst)):
+        frm_2 = img_lst[i][1].replace('1-2x.jpg', '2-2x.jpg')
+        frm_3 = img_lst[i][1].replace('1-2x.jpg', '3-2x.jpg')
+        frm_4 = img_lst[i][1].replace('1-2x.jpg', '4-2x.jpg')
+        frm_5 = img_lst[i][1].replace('1-2x.jpg', '5-2x.jpg')
+        
+        img_lst[i].append(frm_2)
+        img_lst[i].append(frm_3)
+        img_lst[i].append(frm_4)
+        img_lst[i].append(frm_5)
+        
+    return img_lst
+
+
+
+
+import pickle
+
+def picklify_lst(lst, fn):
+    with open(fn+".txt", "wb") as fp:   #Pickling
+        pickle.dump(lst, fp)
+    print('Completed Picklifying.\n\n')
+        
+def pickle_loader(fn):
+    with open(fn, "rb") as fp:   # Unpickling
+        b = pickle.load(fp)
+    print('Completed Loading.\n\n')
+    return b
+        
