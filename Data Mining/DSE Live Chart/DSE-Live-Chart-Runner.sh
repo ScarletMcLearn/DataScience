efcf46b17b27771b299a3acd5484df56d68127c4
Scarlet McLearn <ratul_shams@yahoo.com>
2023-06-25 22:33:39 +0600

    Add files via upload

---
+++ Data Mining/DSE Live Chart/DSE-Live-Chart-Runner.sh
@@ -0,0 +1,5 @@
+source '/mount/Project/Project Files/PythonEnvs/AutomationEnv2/bin/activate'
+
+
+bokeh serve --log-level=debug --show '/mount/Project/Project Files/DSE Live Chart/AddingCompanies/SplitTester7/Adding-Co-4.py' --port 1111
+
